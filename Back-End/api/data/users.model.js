1c4268481160bd646aa34a301a3bcfb150c52c34
Stavroula Iatropoulou <staviatrop@gmail.com>
2019-03-26 12:53:55 +0000

    Added users model and continue on constructing home page

---
+++ Back-End/api/data/users.model.js
@@ -0,0 +1,18 @@
+var mongoose = require('mongoose');
+
+
+var userSchema = new mongoose.Schema( {
+
+    username: {
+        type: String,
+        required: true
+    },
+    password: {
+        type: String,
+        required: true
+    },
+    review_count: Number
+
+})
+
+mongoose.model('User', movieSchema, 'movies');
